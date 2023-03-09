42a180f3af2ac9a788dfb24a3a5dca974e3bf302
daseul <daseul@gmail.com>
2023-03-09 14:19:12 +0900

    초기화

---
+++ js/function.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 4,
+	spaceBetween: 20,
+	slidesPerGroup: 1,
+
+	loop: true,
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+})
